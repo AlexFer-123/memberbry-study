d2142fed4d8e92c4b570f6e9a6a43ccb25ccb2d5
alexandrefer1231 <alexandrefer1231@gmail.com>
2022-08-25 11:41:09 -0300

    crete routes

---
+++ src/pages/register.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="Register">
+        <h1>Register page</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'registerPage'
+    }
+</script>
+
+ <style scoped>
+ 
+ </style>
